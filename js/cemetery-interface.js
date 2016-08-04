47afdb0f9e843f6e02841772edf616a1d14f2a4f
Olivia Hinton and Rachael Bermudes <olivia@>
2016-08-04 08:53:12 -0700

    add geolocation feature

--- js/cemetery-interface.js
+++ js/cemetery-interface.js
@@ -2,16 +2,44 @@ var initMap = require('./../js/cemetery.js').initMapModule;
 
 var apiKey = require('./../.env').apiKey;
 
-$(document).ready(function() {
-   $('#cemeteryLocation').click(function() {
-     var location = $('#location').val();
-     $.get('https://maps.googleapis.com/maps/api/place/radarsearch/json?location=45.5231,-122.6765&radius=500&type=cemetery&key=AIzaSyCvkXpUhaIqG7IaTZwqCKM-sEkBBkrXwLQ').then(function(response) {
-       $('.showCemetery').text("The nearest cemetery to you is " + result.name);
-     }).fail(function(error) {
-       $('.showCemetery').text(error.responseJSON.message);
-     });
-   });
- });
+$( document ).ready(function() {
+  $('#locateUser').click(locateUser);
+});
+
+
+function locateUser() {
+  if (navigator.geolocation){
+    var positionOptions = {
+      enableHighAccuracy: true,
+      timeout: 10 * 1000
+    };
+    navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
+  }
+  else {
+    alert("Your browser doesn't support the Geolocation API");
+  }
+}
+
+
+function geolocationSuccess(position) {
+  var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+  var myOptions = {
+    zoom : 16,
+    center : userLatLng,
+    mapTypeId : google.maps.MapTypeId.ROADMAP
+  };
+
+  var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+  new google.maps.Marker({
+    map: mapObject,
+    position: userLatLng
+  });
+}
+
+function geolocationError(positionError) {
+  alert(positionError);
+}
 
 
 exports.initMapModule = initMap;
